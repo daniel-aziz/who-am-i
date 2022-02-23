c194e9d4b4f9f037afed9aee0ac6c7f5093b0a81
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-02-23 22:05:12 +0200

    final api v1

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.enable('trust proxy')
+
 app.get('/api/whoami', (req, res) => {
-   return res.status(200).json({ ipaddress: req.headers['host'], language: req.headers['accept-language'], software: req.headers['user-agent'] })
+   return res.status(200).json({ ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent'] })
 })
 
 app.listen(3000, () => {
